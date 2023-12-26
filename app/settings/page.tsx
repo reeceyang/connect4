22fa526596cee09fd7293859d18175bea1819a03
Reece Yang <reeceyang@live.com>
2023-12-25 20:05:27 -0600

    remove settings page redirect

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,14 +1,8 @@
 "use client";
 
 import { UserProfile, useAuth } from "@clerk/clerk-react";
-import { useRouter } from "next/router";
 
 export default function Page() {
-  const { isSignedIn } = useAuth();
-  const router = useRouter();
-  if (!isSignedIn) {
-    router.replace("/");
-  }
   return (
     <div className="m-auto w-fit">
       <UserProfile />
